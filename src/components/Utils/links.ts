be2de5ac189cf6960f4945e52cb9cf057ea60d6a
KarolDawidG <karoldawidg@gmail.com>
2023-11-05 10:58:08 +0100

    ssl in backend - https upgradet

--- src/components/Utils/links.ts
+++ src/components/Utils/links.ts
@@ -1,32 +1,32 @@
-export const ENDPOINT_ADMIN = "http://backend-test.ct8.pl/admin";
+export const ENDPOINT_ADMIN = "https://backend-test.ct8.pl/admin";
 
-export const ENDPOINT_AUTH = "http://backend-test.ct8.pl/auth";
+export const ENDPOINT_AUTH = "https://backend-test.ct8.pl/auth";
 
-export const ENDPOINT_USERS = "http://backend-test.ct8.pl/users";
+export const ENDPOINT_USERS = "https://backend-test.ct8.pl/users";
 
-export const ENDPOINT_REGISTER = "http://backend-test.ct8.pl/register";
+export const ENDPOINT_REGISTER = "https://backend-test.ct8.pl/register";
 
-export const ENDPOINT_RESET = "http://backend-test.ct8.pl/reset";
+export const ENDPOINT_RESET = "https://backend-test.ct8.pl/reset";
 
-export const ENDPOINT_EMAIL = "http://backend-test.ct8.pl/forgot";
+export const ENDPOINT_EMAIL = "https://backend-test.ct8.pl/forgot";
 
-export const ENDPOINT_LOGOUT = "http://backend-test.ct8.pl/logout";
+export const ENDPOINT_LOGOUT = "https://backend-test.ct8.pl/logout";
 
 export const LINK_RESET = "https://mega-test.netlify.app/reset";
 
-export const ENDPOINT_REFRESH = "http://backend-test.ct8.pl/auth/refresh";
+export const ENDPOINT_REFRESH = "https://backend-test.ct8.pl/auth/refresh";
 
-export const ENDPOINT_CAPTCHA = `http://backend-test.ct8.pl/cap`;
+export const ENDPOINT_CAPTCHA = `https://backend-test.ct8.pl/cap`;
 
-export const ENDPOINT_DELETE = "http://backend-test.ct8.pl/users/";
+export const ENDPOINT_DELETE = "https://backend-test.ct8.pl/users/";
 
-export const ENDPOINT_UPDATE = "http://backend-test.ct8.pl/users";
+export const ENDPOINT_UPDATE = "https://backend-test.ct8.pl/users";
 
-export const QUIZ = "http://backend-test.ct8.pl/quiz/";
+export const QUIZ = "https://backend-test.ct8.pl/quiz/";
 
-export const QUIZ_20 = "http://backend-test.ct8.pl/quiz-20";
+export const QUIZ_20 = "https://backend-test.ct8.pl/quiz-20";
 
-export const ENDPOINT_CONTACT = 'http://backend-test.ct8.pl/contact-form';
+export const ENDPOINT_CONTACT = 'https://backend-test.ct8.pl/contact-form';
 
 export const ADMIN_ROLE = "admin";
 
